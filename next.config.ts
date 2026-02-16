671204611123b7cac026afd7e6e81f94fa7af410
Tia Wicaksono <wicaksono.tia@gmail.com>
2026-02-16 10:50:40 +0700

    setting default root page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true, // atau false jika redirect sementara
+      },
+    ];
+  },
 };
 
 export default nextConfig;
