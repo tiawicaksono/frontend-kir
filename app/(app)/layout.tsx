a183a02b1e2024e419a14f2d1775f3a77f11b06b
Tia Wicaksono <wicaksono.tia@gmail.com>
2026-02-28 08:08:51 +0700

    animate open page

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,16 +4,13 @@ import "../globals.css";
 import AppMenu from "@/layout/AppMenu";
 import BackToTopButton from "@/components/common/BackToTopButton";
 import SettingsButton from "@/components/common/SettingsButton";
-import RouteTransition from "@/components/layout/RouteTransition";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <BackToTopButton />
       <SettingsButton />
-      <AppMenu>
-        <RouteTransition>{children}</RouteTransition>
-      </AppMenu>
+      <AppMenu>{children}</AppMenu>
     </>
   );
 }
