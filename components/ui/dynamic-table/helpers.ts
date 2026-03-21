ce85432a4b35d8dc95f6d686f632ddfe5185e588
Tia Wicaksono <wicaksono.tia@gmail.com>
2026-03-21 13:36:06 +0700

    column_order

--- components/ui/dynamic-table/helpers.ts
+++ components/ui/dynamic-table/helpers.ts
@@ -2,29 +2,30 @@ export const formatTitle = (key: string) => {
   return key.replace(/_/g, " ").replace(/\b\w/g, (l) => l.toUpperCase());
 };
 
-export const generateColumnsFromData = (data: any[], config?: any) => {
-  if (!data.length) return [];
+export function generateColumnsFromData(data: any[], config: any) {
+  if (!data?.length) return [];
 
-  return Object.keys(data[0])
+  const keys = Object.keys(data[0]);
+
+  // 🔥 ambil order dari BE
+  const order = config?.column_order || [];
+
+  const sortedKeys = [...keys].sort((a, b) => {
+    const indexA = order.indexOf(a);
+    const indexB = order.indexOf(b);
+
+    if (indexA === -1 && indexB === -1) return 0;
+    if (indexA === -1) return 1;
+    if (indexB === -1) return -1;
+
+    return indexA - indexB;
+  });
+
+  return sortedKeys
     .filter((key) => !config?.hidden?.includes(key))
-    .filter((key) => !config?.foreign_keys?.includes(key))
-    .map((key) => {
-      const uniqueValues = [...new Set(data.map((item) => item[key]))].filter(
-        Boolean,
-      );
-
-      return {
-        title: config?.labels?.[key] || formatTitle(key),
-        dataIndex: key,
-        key,
-        sorter: true,
-        // filters:
-        //   uniqueValues.length <= 10
-        //     ? uniqueValues.map((val) => ({
-        //         text: String(val),
-        //         value: val,
-        //       }))
-        //     : null,
-      };
-    });
-};
+    .map((key) => ({
+      title: config?.labels?.[key] || key,
+      dataIndex: key,
+      sorter: true,
+    }));
+}
